04f638d4788aea9fe27309549d9820d9b3fc81c4
Alexey Gutnik <zabqer@gmail.com>
2024-10-01 13:28:55 +0300

    Fixed userlogin form

--- resources/telegramauthorization.special.userlogin.scripts.js
+++ resources/telegramauthorization.special.userlogin.scripts.js
@@ -5,17 +5,17 @@
   // $(".mw-userlogin-rememberme").hide();
   // $("#wpLoginAttempt").hide();
   const hash = window.location.hash;
-  $(".mw-htmlform[name=userlogin]").css({
-    opacity: 0.5,
-    pointerEvents: "none",
-  }).append(
-    $("<div> Выполняется вход </div>").css({
-      position: "absolute",
-      top: "50%",
-      left: "50%",
-    }),
-  );
   if (hash.startsWith("#tgAuthResult")) {
+    $(".mw-htmlform[name=userlogin]").css({
+      opacity: 0.5,
+      pointerEvents: "none",
+    }).append(
+      $("<div> Выполняется вход </div>").css({
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+      }),
+    );
     const tgdata = hash.substring(14);
     $("#mw-input-tgdata").val(tgdata);
     // TODO: this is bad vay to select button

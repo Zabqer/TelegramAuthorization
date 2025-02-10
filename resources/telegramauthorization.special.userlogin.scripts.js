28f08c3d2166abd0f7e7223ffbe7d149262edd4b
Alexey Gutnik <zabqer@gmail.com>
2025-02-10 15:23:02 +0300

    Fixed ability to check remember me checkbox

--- resources/telegramauthorization.special.userlogin.scripts.js
+++ resources/telegramauthorization.special.userlogin.scripts.js
@@ -1,16 +1,26 @@
 (function () {
+  const LS_REMEMBER_CHECKBOX_KEY = "_wiki_auth_remembercheckbox";
   // To hide login form
   // $(".mw-userlogin-username").hide();
   // $(".mw-userlogin-password").hide();
   // $(".mw-userlogin-rememberme").hide();
   // $("#wpLoginAttempt").hide();
+  function setLastCheckboxState(st) {
+    localStorage.setItem(LS_REMEMBER_CHECKBOX_KEY, st ? "true" : "false");
+  }
+  function getLastCheckboxState() {
+    return localStorage.getItem(LS_REMEMBER_CHECKBOX_KEY) === "true";
+  }
+  function dropLastCheckboxState() {
+    localStorage.removeItem(LS_REMEMBER_CHECKBOX_KEY);
+  }
   const hash = window.location.hash;
   if (hash.startsWith("#tgAuthResult")) {
     $(".mw-htmlform[name=userlogin]").css({
       opacity: 0.5,
       pointerEvents: "none",
     }).append(
-      $("<div> Выполняется вход </div>").css({
+      $("<div> Выполняется вход через Telegram... </div>").css({
         position: "absolute",
         top: "50%",
         left: "50%",
@@ -18,7 +28,14 @@
     );
     const tgdata = hash.substring(14);
     $("#mw-input-tgdata").val(tgdata);
-    // TODO: this is bad vay to select button
+    $("#wpRemember").prop("checked", getLastCheckboxState());
+    dropLastCheckboxState();
     $("#mw-input-pluggableauthlogin0").click();
+  } else {
+    const rememberCheckbox = $("#wpRemember");
+    setLastCheckboxState(rememberCheckbox.prop("checked"));
+    rememberCheckbox.on("change", function (event) {
+      setLastCheckboxState(event.target.checked);
+    });
   }
 })();
